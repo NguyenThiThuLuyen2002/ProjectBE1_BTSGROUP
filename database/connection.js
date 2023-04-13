eebe14a39e89ae8a73a93807a77e4b1929fbc4f2
tluyen2602@gmail.com <tluyen2602@gmail.com>
2023-04-13 15:20:03 +0700

    connectmysql

--- database/connection.js
+++ database/connection.js
@@ -4,11 +4,20 @@ connection = mysql.createPool({
     host:"localhost",
     user:"root",
     password:"thuluyen",
-    database:"nodejs"
+    database:"nodejsDB"
 })
+
+connection.getConnection((err,connection) => {
+    if (err)
+        console.log(err)
+    else
+        console.log('connected')
+})
+
 //VALUES(?,?,?)",[name,quantity,category]// truyen tham so
 // connection.query("INSERT INTO Item(itemname,quantity,category) VALUES ('${itemname}', '$")
-connection.query("SELECT * FROM Item", (err, result) =>{
-    console.log(err),
-    console.log(result)
-})
+// connection.query("SELECT * FROM users", (err, result) =>{
+//     console.log(err),
+//     console.log(result)
+// })
+module.exports = connection
---
+++ database/init.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const connect = require('./connection')
+
+
+
+// tạo bảng mới 
+connection.query('CREATE TABLE IF NOT EXISTS users(id INT AUTO_INCREMENT PRIMARY KEY,	fullname NVARCHAR(255) NOT NULL, gender boolean NOT NULL,age INT NOT NULL)', (err, result) => {
+    console.log(err),
+    console.log(result)
+  });
+
+// Thực hiện truy vấn INSERT để thêm người dùng mới
+connection.query("INSERT INTO users (fullname,gender,age) VALUES('Nguyễn Thị Thu',1,20)", (err, result) => {
+    if (err) 
+        console.log(err)
+    else
+        console.log('insert thành công !')
+});
+//xem
+connection.query("SELECT * FROM users", (err, result) =>{
+    console.log(err),
+    console.log(result)
+});
+
+  
+  
